fd7fdfa1aaeaa81271fbf511b125999ed4b73978
mlmosteiro <mosteiro.marialuz@gmailcom>
2021-12-19 13:39:36 +0100

    Challenge1

---
+++ 1/Challenge1.py
@@ -0,0 +1,21 @@
+# abrir y leer archivo
+def parseInput():
+  with open('input.txt', 'r') as f:
+    content = f.read()
+  return content.split('\n')
+
+
+
+def main():
+    list_numbers = parseInput()
+    list_numbers = list(map(int, list_numbers))
+
+    greaters = 0
+
+    for i in range (len(list_numbers)-1):
+        j = i + 1
+        if list_numbers[i] < list_numbers[j]:
+            greaters += 1
+    print(greaters)
+
+main()
