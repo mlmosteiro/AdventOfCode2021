1e3001e8374bca791f508f9f928ad3bb91672fdb
mlmosteiro <mosteiro.marialuz@gmailcom>
2021-12-19 13:39:48 +0100

    Challenge2

---
+++ 2/Challenge2.py
@@ -0,0 +1,25 @@
+# abrir y leer archivo
+def parseInput():
+  with open('input.txt', 'r') as f:
+    content = f.read()
+  return content.split('\n')
+
+def addThree(start, list):
+    return list[start] + list[start+1] + list[start+2]
+
+def main():
+    list_numbers = parseInput()
+    list_numbers = list(map(int, list_numbers))
+
+    greaters = 0
+
+    for i in range (len(list_numbers)-3):
+        j = i + 1
+        firstSet = addThree(i, list_numbers)
+        nextSet = addThree(j, list_numbers)
+
+        if firstSet < nextSet:
+            greaters += 1
+    print(greaters)
+
+main()
