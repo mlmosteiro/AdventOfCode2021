3b29fa4263aa581c19ac1d14eed2eb75ceb9c0b1
mlmosteiro <mosteiro.marialuz@gmailcom>
2021-12-19 14:09:18 +0100

    Challenge3

---
+++ 3/Challenge3.py
@@ -0,0 +1,24 @@
+def parseInputToDictionary():
+  result = {}
+  with open('input.txt', 'r') as f:
+    content = f.read()
+    lines = content.split('\n')
+
+    for line in lines:
+      key, value = line.split(' ')
+      if (key in result.keys()):
+        result[key] +=  int(value)
+      else:
+        result[key] = int( value)
+
+  return result
+
+def main():
+    listOfInstructions = parseInputToDictionary()
+    
+    firstNumber = listOfInstructions["down"] - listOfInstructions["up"]
+    secondNumber = listOfInstructions["forward"]
+
+    print(firstNumber * secondNumber)
+
+main()
