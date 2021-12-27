636d892917c587cf7004672f968f7a0b396ea6e9
mlmosteiro <mosteiro.marialuz@gmailcom>
2021-12-27 23:41:21 +0100

    format code

--- Day7/13/Challenge13.py
+++ Day7/13/Challenge13.py
@@ -2,21 +2,25 @@ from numpy import median
 
 FILE_NAME = "input.txt"
 
+
 def parseFile():
     crabs = []
 
     with open(FILE_NAME, "r") as f:
         content = f.read()
         numbers = content.split(",")
-        crabs = [int(number) for number in numbers]          
+        crabs = [int(number) for number in numbers]
 
     return crabs
+
 
 def getBestPossition(crabs):
     return int(median(crabs))
 
+
 def getConsumedFuel(crab, newPosition):
     return abs(crab - newPosition)
+
 
 def main():
     crabs = parseFile()
